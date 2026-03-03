6e133f5e018ccc684e5cc0694f9f9c7b7f7d048e
Thanh Hương <thanhhuong10102005@gmail.com>
2026-03-03 12:41:18 +0700

    add logic Cộng Hai số

--- CongHaiSo/app/src/main/java/com/example/conghaiso/MainActivity.java
+++ CongHaiSo/app/src/main/java/com/example/conghaiso/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.conghaiso;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText inputA, inputB;
+    TextView kq;
+    Button btnTiepTuc, btnTinhTong, btnThoat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        inputA = findViewById(R.id.inputA);
+        inputB = findViewById(R.id.inputB);
+        kq = findViewById(R.id.kq);
+
+        btnTiepTuc = findViewById(R.id.btnTiepTuc);
+        btnTinhTong = findViewById(R.id.btnTinhTong);
+        btnThoat = findViewById(R.id.btnThoat);
+    }
+
+    public void btnTiepTuc(View view) {
+        inputA.setText("");
+        inputB.setText("");
+        kq.setText("");
+    }
+
+    public void btnTinhTong(View view) {
+        String sa = inputA.getText().toString().trim();
+        String sb = inputB.getText().toString().trim();
+
+        if (sa.isEmpty() || sb.isEmpty()) {
+            Toast.makeText(this, "Vui lòng nhập đủ 2 số!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double a= Double.parseDouble(inputA.getText().toString());
+        double b= Double.parseDouble(inputB.getText().toString());
+
+        double tong= a+b;
+        kq.setText(String.valueOf(tong));
+
+    }
+
+    public void btnThoat(View view) {
+        finish();
     }
 }
