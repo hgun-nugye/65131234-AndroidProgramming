2364112712421f15a72c6ced4f8a9a74b712b30b
Thanh Hương <thanhhuong10102005@gmail.com>
2026-03-26 22:18:17 +0700

    upload RecyclerView part1

---
+++ RecyclerView/app/src/main/java/ntu/bt8/recyclerview/MainActivity.java
@@ -0,0 +1,48 @@
+package ntu.bt8.recyclerview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LanscapeAdapter lanscapeAdapter;
+    ArrayList<Landscape> recyclerViewData;
+    RecyclerView recyclerViewLandscape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        recyclerViewData = getDataForRecyclerView();
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutManager);
+        lanscapeAdapter = new LanscapeAdapter(this, recyclerViewData);
+        recyclerViewLandscape.setAdapter(lanscapeAdapter);
+    }
+
+    //phuong thuc
+    ArrayList<Landscape> getDataForRecyclerView() {
+        ArrayList<Landscape> lstData = new ArrayList<>();
+        lstData.add(new Landscape("nuidoi", "Núi đôi hùng vĩ"));
+        lstData.add(new Landscape("thomong", "Khung cảnh thơ mộng"));
+        lstData.add(new Landscape("troihong", "Bầu trời hồng rực rỡ"));
+        lstData.add(new Landscape("troixanh", "Trời xanh bao la"));
+        return lstData;
+    }
+}
